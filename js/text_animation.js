bbd84a5c1e09fa0c8b78a4ffa451c7eff2c9b8a1
nutri-xca <83430611+nutri-xca@users.noreply.github.com>
2021-04-29 21:57:56 -0400

    Update text_animation.js

--- js/text_animation.js
+++ js/text_animation.js
@@ -7,7 +7,7 @@
 		var animateSpan			= jQuery('.animation_text_word');
 		
 			animateSpan.typed({
-				strings: [" ", "Micronutrient Deficiency", "Agricultural Problems", "Food"],
+				strings: ["Hidden Health Crisis", "Micronutrient Deficiency", "Agricultural Problems", "Food"],
 				loop: true,
 				startDelay: 2,
 				backDelay: 3
